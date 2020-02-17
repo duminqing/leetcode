245a7919819532b74b6753b98087c03c5197d9cf
DMQ <duminqing@outlook.com>
2020-02-17 17:08:12 +0800

    移动0 第二遍

---
+++ src/com/leetcode/N283_2.java
@@ -0,0 +1,39 @@
+package com.leetcode;
+
+public class N283_2 {
+    /**
+     * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+     * <p>
+     * 示例:
+     * <p>
+     * 输入: [0,1,0,3,12]
+     * 输出: [1,3,12,0,0]
+     * 说明:
+     * <p>
+     * 必须在原数组上操作，不能拷贝额外的数组。
+     * 尽量减少操作次数。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/move-zeroes
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+
+    public static void moveZeroes(int[] nums) {
+        for (int i = 0, j = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = tmp;
+                j++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = {0, 3, 0, 4, 0, 5};
+        moveZeroes(a);
+        for (int i : a) {
+            System.out.print(i);
+        }
+    }
+}
