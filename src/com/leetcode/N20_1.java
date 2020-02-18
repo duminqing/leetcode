0d6ba9baa056f6bc263dcdce702b0d025d63a8e3
DMQ <duminqing@outlook.com>
2020-02-18 17:57:59 +0800

    有效的括号

---
+++ src/com/leetcode/N20_1.java
@@ -0,0 +1,56 @@
+package com.leetcode;
+
+import java.util.Stack;
+
+public class N20_1 {
+    /**
+     * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+     * <p>
+     * 有效字符串需满足：
+     * <p>
+     * 左括号必须用相同类型的右括号闭合。
+     * 左括号必须以正确的顺序闭合。
+     * 注意空字符串可被认为是有效字符串。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: "()"
+     * 输出: true
+     * 示例 2:
+     * <p>
+     * 输入: "()[]{}"
+     * 输出: true
+     * 示例 3:
+     * <p>
+     * 输入: "(]"
+     * 输出: false
+     * 示例 4:
+     * <p>
+     * 输入: "([)]"
+     * 输出: false
+     * 示例 5:
+     * <p>
+     * 输入: "{[]}"
+     * 输出: true
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/valid-parentheses
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public static boolean isValid(String s) {
+        char[] array = s.toCharArray();
+        Stack<Character> stack = new Stack();
+        for (char c : array) {
+            if (!stack.isEmpty() && ((c == ')' && stack.peek() == '(') || (c == ']' && stack.peek() == '[') || (c == '}' && stack.peek() == '{'))) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValid("([)]"));
+    }
+}
