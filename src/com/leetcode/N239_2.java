ebe09cdcb67fe0654949912080f0d2b63adbbc1e
DMQ <duminqing@outlook.com>
2020-02-20 18:06:53 +0800

    239. 第二次 滑动窗口最大值

--- src/com/leetcode/N239_2.java
+++ src/com/leetcode/N239_2.java
@@ -6,11 +6,18 @@ public class N239_2 {
 
 
     public int[] maxSlidingWindow(int[] nums, int k) {
+        int length = nums.length;
+        if (length * k == 0) {
+            return new int[0];
+        }
+        if (k == 1) {
+            return nums;
+        }
         int[] max = new int[nums.length - k + 1];
         int j = 0;
         ArrayDeque<Integer> deque = new ArrayDeque<>();
         for (int i = 0; i < nums.length; i++) {
-            while (!deque.isEmpty() && deque.getLast() <= nums[i]) {
+            while (!deque.isEmpty() && nums[deque.getLast()] <= nums[i]) {
                 deque.pollLast();
             }
             deque.addLast(i);
