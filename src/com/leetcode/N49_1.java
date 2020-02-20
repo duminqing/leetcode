3adbbde48a62cea54e2326b8bf4fa77c3f09b505
DMQ <duminqing@outlook.com>
2020-02-20 11:10:38 +0800

    1. 两数之和

---
+++ src/com/leetcode/N49_1.java
@@ -0,0 +1,24 @@
+package com.leetcode;
+
+import java.util.*;
+
+public class N49_1 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String s0 = new String(chars);
+            if (map.containsKey(s0)) {
+                map.get(s0).add(s);
+            } else {
+                List<String> list = new ArrayList<>();
+                list.add(s);
+                map.put(s0, list);
+
+            }
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
