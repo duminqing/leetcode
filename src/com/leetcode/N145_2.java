67d15d7e171eda8f632f524cdca8aeae83343030
DMQ <duminqing@outlook.com>
2020-02-20 14:03:44 +0800

    145. 第二次 后序遍历二叉树

---
+++ src/com/leetcode/N145_2.java
@@ -0,0 +1,31 @@
+package com.leetcode;
+
+import com.leetcode.bean.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+
+public class N145_2 {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        if (root == null) {
+            return Collections.emptyList();
+        }
+        List<Integer> valueList = new ArrayList<>();
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode current = stack.pop();
+            if (current.left != null) {
+                stack.push(current.left);
+            }
+            if (current.right != null) {
+                stack.push(current.right);
+            }
+            valueList.add(0, current.val);
+        }
+        return valueList;
+    }
+}
