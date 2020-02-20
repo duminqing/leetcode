bc546640802489e65510c7868793d47a2c8539c2
DMQ <duminqing@outlook.com>
2020-02-20 14:26:03 +0800

    94. 第一次 种序遍历二叉树

---
+++ src/com/leetcode/N94_2.java
@@ -0,0 +1,32 @@
+package com.leetcode;
+
+import com.leetcode.bean.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+
+public class N94_2 {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) {
+            return Collections.emptyList();
+        }
+        List<Integer> valueList = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+
+        while (current != null || !stack.isEmpty()) {
+
+            if (current != null) {
+                stack.push(current);
+                current = current.left;
+            } else {
+                current = stack.pop();
+                valueList.add(current.val);
+                current = current.right;
+            }
+        }
+        return valueList;
+    }
+}
