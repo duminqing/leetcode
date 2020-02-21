58d6157ff923ff79c1b5a95ce626fe9b0d37b444
DMQ <duminqing@outlook.com>
2020-02-21 09:34:35 +0800

    590. 第一次 N叉树的后序遍历

---
+++ src/com/leetcode/N590_1.java
@@ -0,0 +1,39 @@
+package com.leetcode;
+
+import com.leetcode.bean.Node;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+
+public class N590_1 {
+    /**
+     * 给定一个 N 叉树，返回其节点值的后序遍历。
+     * <p>
+     * 例如，给定一个 3叉树 :
+     * <p>
+     * 返回其后序遍历: [5,6,3,2,4,1].
+     */
+
+    public List<Integer> postorder(Node root) {
+        if (root == null) {
+            return Collections.emptyList();
+        }
+        List<Integer> valueList = new ArrayList<>();
+
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.empty()) {
+            Node node = stack.pop();
+            if (node.children != null && node.children.size() > 0) {
+                for (Node n : node.children) {
+                    stack.push(n);
+                }
+            }
+            valueList.add(0, node.val);
+        }
+
+        return valueList;
+    }
+}
