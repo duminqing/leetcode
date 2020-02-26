62de2b1608a71308995cce0e2fed953257ddc2c6
DMQ <duminqing@outlook.com>
2020-02-26 09:49:46 +0800

    98. 搜索二叉树 第二次 111. 树的最小深度 第一次

---
+++ src/com/leetcode/N98_2.java
@@ -0,0 +1,45 @@
+package com.leetcode;
+
+import com.leetcode.bean.TreeNode;
+
+public class N98_2 {
+    public static void main(String[] args) {
+        System.out.println(true && false);
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        return helper(root, null, null);
+    }
+
+    private boolean helper(TreeNode root, Integer lower, Integer upper) {
+        if (root == null) {
+            return true;
+        }
+        int val = root.val;
+        if (lower != null && lower >= val) {
+            return false;
+        }
+        if (upper != null && upper <= val) {
+            return false;
+        }
+        if (!helper(root.right, val, upper)) {
+            return false;
+        }
+        if (!helper(root.left, lower, val)) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean helper2(TreeNode root, Integer lower, Integer upper) {
+        if (root == null) {
+            return true;
+        }
+        int val = root.val;
+        return (lower == null || lower < val)
+                && (upper == null || upper > val)
+                && helper(root.right, val, upper)
+                && helper(root.left, lower, val);
+    }
+
+}
