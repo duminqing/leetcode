761c2bcbeeb413687ea396eead1c43fdd251c58c
DMQ <duminqing@outlook.com>
2020-02-27 09:38:02 +0800

    46. 全排列

---
+++ src/com/leetcode/N46_2.java
@@ -0,0 +1,47 @@
+package com.leetcode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class N46_2 {
+    /**
+     * 46. 全排列
+     * 给定一个没有重复数字的序列，返回其所有可能的全排列。
+     * <p>
+     * 示例:
+     * <p>
+     * 输入: [1,2,3]
+     * 输出:
+     * [
+     * [1,2,3],
+     * [1,3,2],
+     * [2,1,3],
+     * [2,3,1],
+     * [3,1,2],
+     * [3,2,1]
+     * ]
+     */
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> list = new LinkedList<>();
+
+        helper(nums, list, new LinkedList<>());
+        return list;
+
+    }
+
+    private void helper(int[] nums, List<List<Integer>> resultList, LinkedList<Integer> list) {
+
+        if (list.size() == nums.length) {
+            resultList.add(new LinkedList<>(list));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (!list.contains(nums[i])) {
+                list.addLast(nums[i]);
+                helper(nums, resultList, list);
+                list.removeLast();
+            }
+        }
+    }
+}
