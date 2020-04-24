cb1452b47a7f63ba7d6589bbb5d2389b1a472d85
DMQ <duminqing@outlook.com>
2020-04-24 17:48:06 +0800

    测试SynchronousQueue

---
+++ src/com/java/question/TestCaseSynchronousBlockingQueue.java
@@ -0,0 +1,34 @@
+package com.java.question;
+
+import java.util.concurrent.*;
+
+public class TestCaseSynchronousBlockingQueue {
+    public static void main(String[] args) throws InterruptedException {
+        SynchronousQueue<Integer> queue = new SynchronousQueue<>(true);
+        ExecutorService executorService = new ThreadPoolExecutor(10, 10, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue());
+        for (int i = 0; i < 5; i++) {
+            Thread.sleep(1000);
+
+            int finalI = i;
+            executorService.submit(() -> {
+                try {
+                    queue.put(finalI);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        for (int i = 0; i < 5; i++) {
+            Thread.sleep(1000);
+
+            executorService.submit(() -> {
+                try {
+                    int a = queue.take();
+                    System.out.println(a);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+    }
+}
