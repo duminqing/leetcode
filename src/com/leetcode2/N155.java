9c8d9d68f466b1f0f3247c7d8574f0ad3541b96d
DMQ <duminqing@outlook.com>
2020-05-12 09:05:39 +0800

    双栈求最小栈

---
+++ src/com/leetcode2/N155.java
@@ -0,0 +1,85 @@
+package com.leetcode2;
+
+import java.util.Stack;
+
+/**
+ * 155. 最小栈
+ * 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+ * <p>
+ * push(x) —— 将元素 x 推入栈中。
+ * pop() —— 删除栈顶的元素。
+ * top() —— 获取栈顶元素。
+ * getMin() —— 检索栈中的最小元素。
+ * <p>
+ * <p>
+ * 示例:
+ * <p>
+ * 输入：
+ * ["MinStack","push","push","push","getMin","pop","top","getMin"]
+ * [[],[-2],[0],[-3],[],[],[],[]]
+ * <p>
+ * 输出：
+ * [null,null,null,null,-3,null,0,-2]
+ * <p>
+ * 解释：
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> 返回 -3.
+ * minStack.pop();
+ * minStack.top();      --> 返回 0.
+ * minStack.getMin();   --> 返回 -2.
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * pop、top 和 getMin 操作总是在 非空栈 上调用。
+ */
+public class N155 {
+
+    class MinStack {
+
+        /**
+         * initialize your data structure here.
+         */
+        private Stack<Integer> stack;
+        private Stack<Integer> minStack;
+
+        public MinStack() {
+            stack = new Stack<>();
+            minStack = new Stack<>();
+        }
+
+        public void push(int x) {
+            stack.push(x);
+            if (minStack.isEmpty() || minStack.peek() >= x) {
+                minStack.push(x);
+            }
+        }
+
+        public void pop() {
+
+            if (stack.pop().equals(minStack.peek())) {
+                minStack.pop();
+            }
+        }
+
+        public int top() {
+            return stack.peek();
+        }
+
+        public int getMin() {
+            return minStack.peek();
+        }
+    }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+}
