1be0fb8b229b1db41c3a98a76076203ca9bae9b8
DMQ <duminqing@outlook.com>
2020-06-11 15:47:25 +0800

    倒叙入栈解

---
+++ src/com/leetcode2/N739.java
@@ -0,0 +1,29 @@
+package com.leetcode2;
+
+import java.util.Stack;
+
+/**
+ * 739. 每日温度
+ * 根据每日 气温 列表，请重新生成一个列表，对应位置的输出是需要再等待多久温度才会升高超过该日的天数。如果之后都不会升高，请在该位置用 0 来代替。
+ * <p>
+ * 例如，给定一个列表 temperatures = [73, 74, 75, 71, 69, 72, 76, 73]，你的输出应该是 [1, 1, 4, 2, 1, 1, 0, 0]。
+ * <p>
+ * 提示：气温 列表长度的范围是 [1, 30000]。每个气温的值的均为华氏度，都是在 [30, 100] 范围内的整数。
+ */
+public class N739 {
+
+    static class Solution {
+        public int[] dailyTemperatures(int[] T) {
+            int[] days = new int[T.length];
+            Stack<Integer> stack = new Stack<>();
+            for (int i = T.length - 1; i >= 0; i--) {
+                while (!stack.isEmpty() && T[i] >= T[stack.peek()]) {
+                    stack.pop();
+                }
+                days[i] = stack.isEmpty() ? 0 : stack.peek() - i;
+                stack.push(i);
+            }
+            return days;
+        }
+    }
+}
